5e4aa980215b6e82ebc0acfa04237e36dee7776e
rensPols <rens@cepols.nl>
2024-02-21 10:51:16 +0100

    Added javadoc to TotalDebt.java

--- commons/src/main/java/commons/TotalDebt.java
+++ commons/src/main/java/commons/TotalDebt.java
@@ -10,6 +10,10 @@ public class TotalDebt {
         // TODO: create TotalDebt
     }
 
+    /** Checks if the Object that is provided is equal to this TotalDebt object
+     * @param o The Object that has to be compared to this TotalDebt Object
+     * @return true if they are equal, false when they are not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -18,6 +22,9 @@ public class TotalDebt {
         return Double.compare(toBePaid, totalDebt.toBePaid) == 0 && Objects.equals(currency, totalDebt.currency);
     }
 
+    /** Provides a hash for the current Object
+     * @return the hash of this Object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(toBePaid, currency);
