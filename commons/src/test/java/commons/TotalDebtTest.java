7ed8b6c3dd5c89a4507af846109398c910a65569
Wing <w.p.wong@student.tudelft.nl>
2024-02-23 18:38:29 +0100

    Init skeleton test class

--- commons/src/test/java/commons/TotalDebtTest.java
+++ commons/src/test/java/commons/TotalDebtTest.java
@@ -1,7 +1,53 @@
 package commons;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TotalDebtTest {
 
+    @Test
+    void getToBePaid() {
+
+    }
+
+    @Test
+    void setToBePaid() {
+    }
+
+    @Test
+    void getCurrency() {
+    }
+
+    @Test
+    void setCurrency() {
+    }
+
+    @Test
+    void getReceiver() {
+    }
+
+    @Test
+    void setReceiver() {
+    }
+
+    @Test
+    void getNoParticipants() {
+    }
+
+    @Test
+    void setNoParticipants() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
