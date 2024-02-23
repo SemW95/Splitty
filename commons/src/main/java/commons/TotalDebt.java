3f9152dd833fa40889a3df366996b4409fb13196
Wing <w.p.wong@student.tudelft.nl>
2024-02-23 17:54:42 +0100

    Initialisation of the TotalDebt class.

--- commons/src/main/java/commons/TotalDebt.java
+++ commons/src/main/java/commons/TotalDebt.java
@@ -1,33 +1,74 @@
 package commons;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 public class TotalDebt {
     BigDecimal toBePaid;
     Currency currency;
+    Person receiver;
+    List<Person> noParticipants;
 
-    public TotalDebt(double toBePaid, Currency currency) {
-        // TODO: create TotalDebt
+    public TotalDebt(BigDecimal toBePaid, Currency currency, Person receiver, List<Person> noParticipants) {
+        this.toBePaid = toBePaid;
+        this.currency = currency;
+        this.receiver = receiver;
+        this.noParticipants = noParticipants;
     }
 
-    /** Checks if the Object that is provided is equal to this TotalDebt object
-     * @param o The Object that has to be compared to this TotalDebt Object
-     * @return true if they are equal, false when they are not
-     */
+    public BigDecimal getToBePaid() {
+        return toBePaid;
+    }
+
+    public void setToBePaid(BigDecimal toBePaid) {
+        this.toBePaid = toBePaid;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Person getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Person receiver) {
+        this.receiver = receiver;
+    }
+
+    public List<Person> getNoParticipants() {
+        return noParticipants;
+    }
+
+    public void setNoParticipants(List<Person> noParticipants) {
+        this.noParticipants = noParticipants;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TotalDebt totalDebt = (TotalDebt) o;
-        return Objects.equals(toBePaid, totalDebt.toBePaid) && Objects.equals(currency, totalDebt.currency);
+        return Objects.equals(toBePaid, totalDebt.toBePaid) && Objects.equals(currency, totalDebt.currency) && Objects.equals(receiver, totalDebt.receiver) && Objects.equals(noParticipants, totalDebt.noParticipants);
     }
 
-    /** Provides a hash for the current Object
-     * @return the hash of this Object
-     */
     @Override
     public int hashCode() {
-        return Objects.hash(toBePaid, currency);
+        return Objects.hash(toBePaid, currency, receiver, noParticipants);
+    }
+
+    @Override
+    public String toString() {
+        return "TotalDebt{" +
+                "toBePaid=" + toBePaid +
+                ", currency=" + currency +
+                ", receiver=" + receiver +
+                ", noParticipants=" + noParticipants +
+                '}';
     }
 }
