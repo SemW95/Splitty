1a96b35327acb0a782e796e4e7730a9e8768c5d0
Wing <w.p.wong@student.tudelft.nl>
2024-03-02 22:09:07 +0100

    finalise all tests and change person jpa repository to use optionals

--- server/src/main/java/server/database/PersonRepository.java
+++ server/src/main/java/server/database/PersonRepository.java
@@ -2,14 +2,16 @@ package server.database;
 
 import commons.Person;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- *  Repository Interface for the Person Class.
+ * Repository Interface for the Person Class.
  */
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
-    Person findById(long id);
+    Optional<Person> findById(long id);
 
     List<Person> findByFirstName(String firstName);
+
+    List<Person> findPeopleByFirstNameContainingIgnoreCase(String firstName);
 }
