b63a2a065597d83cfde7f3d50363f0f05515fa05
Xiaoyu Du <X.Du-9@student.tudelft.nl>
2024-03-02 15:40:56 +0100

    Some preparation in ColourRepository.

---
+++ server/src/main/java/server/database/ColourRepository.java
@@ -0,0 +1,10 @@
+package server.database;
+
+import commons.Colour;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The repository of the currency entity.
+ */
+public interface ColourRepository extends JpaRepository<Colour, Long> {
+}
