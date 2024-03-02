cc1560b04c233fcdf1527d36c4692592bde24c29
Lukas Milieška <l.milieska@student.tudelft.nl>
2024-03-02 20:44:58 +0100

    Add seeder class

---
+++ server/src/main/java/server/Seeder.java
@@ -0,0 +1,44 @@
+package server;
+
+import commons.Currency;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import server.database.CurrencyRepository;
+
+/**
+ * This a seeder class for initializing default values.
+ * It is run everytime the server is started up.
+ */
+@Component
+public class Seeder implements CommandLineRunner {
+    private final CurrencyRepository currencyRepository;
+
+    public Seeder(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        loadCurrencies();
+    }
+
+    /**
+     * Creates the default currencies.
+     */
+    private void loadCurrencies() {
+        // Check if there are no currencies
+        if (currencyRepository.count() == 0) {
+            System.out.println("Adding default currencies...");
+
+            Currency euro = new Currency("Euro", "EUR", '€');
+            Currency dollar = new Currency("Dollar", "USD", '$');
+            Currency swiss = new Currency("Swiss franc", "CHF", 'F');
+
+            currencyRepository.save(euro);
+            currencyRepository.save(dollar);
+            currencyRepository.save(swiss);
+        }
+    }
+
+    // TODO: create loader for tags
+}
