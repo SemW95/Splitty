5b408508d7ec2c3b0915494991c073f936e86d1f
Xiaoyu Du <X.Du-9@student.tudelft.nl>
2024-03-03 21:51:49 +0100

    Delete some useless comments.

--- server/src/main/java/server/database/ColourRepository.java
+++ server/src/main/java/server/database/ColourRepository.java
@@ -9,8 +9,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ColourRepository extends JpaRepository<Colour, Long> {
     Optional<Colour> findColourById(long id);
-    // Do we need to add findColourByIndex and findColourByHexString?
-    // I tried to add them, but they cannot be added just like id because the union of three index
-    // and the HexString are not variables in Colour.
-
 }
