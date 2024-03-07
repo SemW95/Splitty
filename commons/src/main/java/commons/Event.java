d2f872a7b09f73e907c849c77bdcebe4a94ed2af
Rens Pols <G.J.Pols@student.tudelft.nl>
2024-03-07 16:58:54 +0100

    Changed creationDate to startDate, endDate & lastModifiedDateTime

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -34,10 +35,14 @@ public class Event {
     @OneToMany
     List<Payment> payments;
     @Column(columnDefinition = "TIMESTAMP")
-    Instant creationDate;
+    LocalDate startDate;
+    @Column(columnDefinition = "TIMESTAMP")
+    LocalDate endDate;
+    @Column(columnDefinition = "TIMESTAMP")
+    Instant lastModifiedDateTime;
 
     /**
-     * Creates a new Event.
+     * Creates a new Event without dates.
      *
      * @param title       The title of the Event.
      * @param description The description of the Event.
@@ -46,18 +51,40 @@ public class Event {
         String title,
         String description
     ) {
-        this.code = generateInviteCode();
-        this.title = title;
-        this.description = description;
-        this.people = new ArrayList<Person>();
-        // TODO: add the three standard Tags (extended feature)
-        this.tags = new ArrayList<Tag>();
-        this.expenses = new ArrayList<Expense>();
-        this.payments = new ArrayList<Payment>();
+        this(
+            title,
+            description,
+            new ArrayList<Tag>(),
+            LocalDate.now(),
+            LocalDate.now()
+        );
     }
 
     /**
-     * Creates a new Event with predefined Tags.
+     * Creates a new Event.
+     *
+     * @param title       The title of the Event.
+     * @param description The description of the Event.
+     * @param startDate    The date that this Event started.
+     * @param endDate      The date that this Event ended.
+     */
+    public Event(
+        String title,
+        String description,
+        LocalDate startDate,
+        LocalDate endDate
+    ) {
+        this(
+            title,
+            description,
+            new ArrayList<Tag>(),
+            startDate,
+            endDate
+        );
+    }
+
+    /**
+     * Creates a new Event with predefined Tags and without dates.
      *
      * @param title       The title of the Event.
      * @param description The description of the Event.
@@ -68,19 +95,84 @@ public class Event {
         String description,
         ArrayList<Tag> tags
     ) {
-        this.code = generateInviteCode();
-        this.title = title;
-        this.description = description;
-        this.people = new ArrayList<Person>();
-        this.tags = tags;
-        this.expenses = new ArrayList<Expense>();
-        this.payments = new ArrayList<Payment>();
+        this(
+            title,
+            description,
+            new ArrayList<Person>(),
+            tags,
+            new ArrayList<Expense>(),
+            new ArrayList<Payment>(),
+            LocalDate.now(),
+            LocalDate.now()
+        );
+    }
+
+    /**
+     * Creates a new Event with predefined Tags.
+     *
+     * @param title       The title of the Event.
+     * @param description The description of the Event.
+     * @param tags        The Tags of the Event
+     * @param startDate    The date that this Event started.
+     * @param endDate      The date that this Event ended.
+     */
+    public Event(
+        String title,
+        String description,
+        ArrayList<Tag> tags,
+        LocalDate startDate,
+        LocalDate endDate
+    ) {
+        this(
+            title,
+            description,
+            new ArrayList<Person>(),
+            tags,
+            new ArrayList<Expense>(),
+            new ArrayList<Payment>(),
+            startDate,
+            endDate
+        );
     }
 
     /**
      * Empty constructor for JPA.
      */
     protected Event() {
+    }
+
+    /**
+     * The Event constructor that is the base for all non-import constructors.
+     *
+     * @param title        The Event title.
+     * @param description  The Event description.
+     * @param people       The ArrayList with all Persons in the Event.
+     * @param tags         The ArrayList with all the Tags in the Event.
+     * @param expenses     The ArrayList with all the Expenses in the Event.
+     * @param payments     The ArrayList with all the Payments in the Event.
+     * @param startDate    The date that this Event started.
+     * @param endDate      The date that this Event ended.
+     */
+    public Event(
+        String title,
+        String description,
+        List<Person> people,
+        List<Tag> tags,
+        List<Expense> expenses,
+        List<Payment> payments,
+        LocalDate startDate,
+        LocalDate endDate
+    ) {
+        this.code = generateInviteCode();
+        this.title = title;
+        this.description = description;
+        this.people = people;
+        this.tags = tags;
+        this.expenses = expenses;
+        this.payments = payments;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        updateLastModifiedDateTime();
     }
 
     /**
@@ -92,11 +184,21 @@ public class Event {
      * @param tags         The ArrayList with all the Tags in the Event.
      * @param expenses     The ArrayList with all the Expenses in the Event.
      * @param payments     The ArrayList with all the Payments in the Event.
-     * @param creationDate Creation date of the Event.
+     * @param startDate    The date that this Event started.
+     * @param endDate      The date that this Event ended.
+     * @param lastModifiedDateTime The date of when this Event was last modified.
      */
-    public Event(String title, String description, List<Person> people,
-                 List<Tag> tags, List<Expense> expenses, List<Payment> payments,
-                 Instant creationDate) {
+    public Event(
+        String title,
+        String description,
+        List<Person> people,
+        List<Tag> tags,
+        List<Expense> expenses,
+        List<Payment> payments,
+        LocalDate startDate,
+        LocalDate endDate,
+        Instant lastModifiedDateTime
+    ) {
         this.code = generateInviteCode();
         this.title = title;
         this.description = description;
@@ -104,14 +206,21 @@ public class Event {
         this.tags = tags;
         this.expenses = expenses;
         this.payments = payments;
-        this.creationDate = creationDate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.lastModifiedDateTime = lastModifiedDateTime;
     }
 
     /**
      * A UUID is not unique, however the chance of having duplicates is minimal.
      */
     private static String generateInviteCode() {
+        // TODO: check if it already exists
         return UUID.randomUUID().toString();
+    }
+
+    public void updateLastModifiedDateTime() {
+        // TODO: update the lastModifiedDateTime variable
     }
 
     /**
@@ -128,32 +237,17 @@ public class Event {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-
         Event event = (Event) o;
-
-        if (!Objects.equals(code, event.code)) {
-            return false;
-        }
-        if (!Objects.equals(title, event.title)) {
-            return false;
-        }
-        if (!Objects.equals(description, event.description)) {
-            return false;
-        }
-        if (!Objects.equals(people,
-            event.people)) {
-            return false;
-        }
-        if (!Objects.equals(tags, event.tags)) {
-            return false;
-        }
-        if (!Objects.equals(expenses, event.expenses)) {
-            return false;
-        }
-        if (!Objects.equals(payments, event.payments)) {
-            return false;
-        }
-        return Objects.equals(creationDate, event.creationDate);
+        return id == event.id && Objects.equals(code, event.code)
+            && Objects.equals(title, event.title)
+            && Objects.equals(description, event.description)
+            && Objects.equals(people, event.people)
+            && Objects.equals(tags, event.tags)
+            && Objects.equals(expenses, event.expenses)
+            && Objects.equals(payments, event.payments)
+            && Objects.equals(startDate, event.startDate)
+            && Objects.equals(endDate, event.endDate)
+            && Objects.equals(lastModifiedDateTime, event.lastModifiedDateTime);
     }
 
     /**
@@ -163,7 +257,9 @@ public class Event {
      */
     @Override
     public int hashCode() {
-        return Objects.hash(code, title, description, tags, expenses, payments, creationDate);
+        return Objects.hash(id, code, title, description, people, tags, expenses, payments,
+            startDate,
+            endDate, lastModifiedDateTime);
     }
 
     public long getId() {
@@ -226,12 +322,12 @@ public class Event {
         this.payments = payments;
     }
 
-    public Instant getCreationDate() {
-        return creationDate;
+    public Instant getLastModifiedDateTime() {
+        return lastModifiedDateTime;
     }
 
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
+    public void setLastModifiedDateTime(Instant creationDate) {
+        this.lastModifiedDateTime = creationDate;
     }
 
 }
