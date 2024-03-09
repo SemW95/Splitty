328f659c9ac6c617857358138d08c4b5311adcd9
Eva Miesen <e.a.p.miesen@student.tudelft.nl>
2024-03-09 20:24:49 +0100

    Added tests

--- commons/src/test/java/commons/CurrencyTest.java
+++ commons/src/test/java/commons/CurrencyTest.java
@@ -3,15 +3,27 @@ package commons;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // TODO: https://gitlab.ewi.tudelft.nl/cse1105/2023-2024/teams/oopp-team-25/-/issues/15
 class CurrencyTest {
+
+    private Currency euro;
+    private Currency euro2;
+    private Currency dollar;
+    private Currency swiss;
+
+    @BeforeEach
+    void setUp() {
+        euro = new Currency("Euro", "EUR", '€');
+        euro2 = new Currency("Euro", "EUR", '€');
+        dollar = new Currency("Dollar", "USD", '$');
+        swiss = new Currency("Swiss franc", "CHF", 'F');
+    }
+
     @Test
     void testGetConversionRate() {
-        Currency euro = new Currency("Euro", "EUR", '€');
-        Currency dollar = new Currency("Dollar", "USD", '$');
-        Currency swiss = new Currency("Swiss franc", "CHF", 'F');
 
         BigDecimal eurToUsd = euro.getConversionRate(dollar, "2024-01-01");
         assertEquals(new BigDecimal("1.105"), eurToUsd);
@@ -19,4 +31,51 @@ class CurrencyTest {
         BigDecimal usdToSwiss = dollar.getConversionRate(swiss, "2024-01-01");
         assertEquals(new BigDecimal("0.83801"), usdToSwiss);
     }
+
+    @Test
+    void testEquals() {
+        assertEquals(euro, euro2);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(euro.hashCode(), euro2.hashCode(), "Hash codes should be equal");
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Euro", euro.getName(), "Incorrect name");
+    }
+
+    @Test
+    void setName() {
+        String newName = "NotEuroAnymore";
+        euro.setName(newName);
+        assertEquals(newName, euro.getName(), "Setting name failed");
+    }
+
+    @Test
+    void getCode() {
+        assertEquals("EUR", euro.getCode(), "Incorrect code");
+    }
+
+    @Test
+    void setCode() {
+        String newCode = "NotEURAnymore";
+        euro.setCode(newCode);
+        assertEquals(newCode, euro.getCode(), "Setting code failed");
+    }
+
+    @Test
+    void getSymbol() {
+        assertEquals('€', euro.getSymbol(), "Incorrect symbol");
+    }
+
+    @Test
+    void setSymbol() {
+        char newSymbol = '$';
+        euro.setSymbol(newSymbol);
+        assertEquals('$', euro.getSymbol());
+    }
+
 }
