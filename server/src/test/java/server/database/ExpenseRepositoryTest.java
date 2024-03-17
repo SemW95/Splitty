3138b326f5473251f5781736408c2663b3e8b241
Wing Wong <w.p.wong@student.tudelft.nl>
2024-03-18 00:17:25 +0100

    fixed the cast error by persisting absolutely every object in the testing and by using a list implementation in the expense class

--- server/src/test/java/server/database/ExpenseRepositoryTest.java
+++ server/src/test/java/server/database/ExpenseRepositoryTest.java
@@ -1,9 +1,7 @@
 package server.database;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import commons.Colour;
 import commons.Expense;
@@ -34,22 +32,32 @@ class ExpenseRepositoryTest {
     private ColourRepository colourRepository;
 
     private Expense testExpense1;
+    private Expense testExpense2;
 
 
     @BeforeEach
     void setUp() {
-        Person testPerson1 = new Person("Alice", "Hennessy", "aliceh@domain.name", "NL80RABO3330533676", "ABNANL2A");
-        Person testPerson2 = new Person("Bobertus", "Fireball", "bobf@domain.name", "NL92RABO4452759149", "AGBLLT2X");
-        Person testPerson3 = new Person("Charlie", "Smith", "charlies@domain.name", "GB94BARC10201530093459", "BOFAUS3N");
-        Person testPerson4 = new Person("David", "Brown", "davidb@domain.name", "NL18ABNA2262368678", "DEUTDEFF");
-        Person testPerson5 = new Person("Eve", "Johnson", "evej@domain.name", "NL24RABO8487376045", "BNPAFRPP");
-
         List<Person> participants1 = new ArrayList<>();
+        Person testPerson1 = new Person("Alice", "Hennessy",
+            "aliceh@domain.name", "NL80RABO3330533676", "ABNANL2A");
+        Person testPerson3 = new Person("Charlie", "Smith",
+            "charlies@domain.name", "GB94BARC10201530093459", "BOFAUS3N");
+        Person testPerson4 = new Person("David", "Brown",
+            "davidb@domain.name", "NL18ABNA2262368678", "DEUTDEFF");
+        Person testPerson5 = new Person("Eve", "Johnson",
+            "evej@domain.name", "NL24RABO8487376045", "BNPAFRPP");
         participants1.add(testPerson1);
         participants1.add(testPerson3);
         participants1.add(testPerson4);
         participants1.add(testPerson5);
-        personRepository.saveAll(List.of(testPerson1, testPerson2, testPerson3, testPerson4, testPerson5));
+        List<Person> participants2 = new ArrayList<>();
+        participants2.add(testPerson1);
+        participants2.add(testPerson3);
+        participants2.add(testPerson4);
+        Person testPerson2 = new Person("Bobertus", "Fireball",
+            "bobf@domain.name", "NL92RABO4452759149", "AGBLLT2X");
+        personRepository.saveAll(
+            List.of(testPerson1, testPerson2, testPerson3, testPerson4, testPerson5));
 
         BigDecimal paid1 = BigDecimal.valueOf(250);
 
@@ -57,13 +65,17 @@ class ExpenseRepositoryTest {
         colourRepository.saveAll(List.of(faintSunshine));
 
 
-        Tag tag1 = new Tag("Faint Sunshine", faintSunshine );
+        Tag tag1 = new Tag("Faint Sunshine", faintSunshine);
         tagRepository.saveAll(List.of(tag1));
 
 
         Instant paymentDateTime1 = Instant.ofEpochMilli(712000712);
 
-        testExpense1 = new Expense("pay for drinks", participants1, testPerson2, paid1, tag1, paymentDateTime1);
+        testExpense1 = new Expense("pay for drinks", participants1, testPerson2, paid1, tag1,
+            paymentDateTime1);
+        testExpense2 =
+            new Expense("get completely numb to the pain of the void", participants2, testPerson5,
+                paid1, tag1, paymentDateTime1);
 
         expenseRepository.save(testExpense1);
     }
@@ -88,8 +100,22 @@ class ExpenseRepositoryTest {
     }
 
     @Test
-    void testFindByDescription() {
-        List<Expense> elist1 = expenseRepository.findByDescriptionContainingIgnoreCase(testExpense1.getDescription());
+    void testFindByIdNotFirstId() {
+        Expense e2 = expenseRepository.findById(testExpense1.getId()).orElse(null);
+        assertNotNull(e2);
+
+        assertEquals(testExpense1.getDescription(), e2.getDescription());
+        assertEquals(testExpense1.getPaid(), e2.getPaid());
+        assertEquals(testExpense1.getReceiver(), e2.getReceiver());
+        assertEquals(testExpense1.getParticipants(), e2.getParticipants());
+        assertEquals(testExpense1.getDescription(), e2.getDescription());
+        assertEquals(testExpense1.getPaymentDateTime(), e2.getPaymentDateTime());
+    }
+
+    @Test
+    void testFindByFullDescription() {
+        List<Expense> elist1 =
+            expenseRepository.findByDescriptionContainingIgnoreCase(testExpense1.getDescription());
 
         Expense e1 = elist1.get(0);
         assertNotNull(e1);
@@ -100,5 +126,22 @@ class ExpenseRepositoryTest {
         assertEquals(testExpense1.getParticipants(), e1.getParticipants());
         assertEquals(testExpense1.getDescription(), e1.getDescription());
         assertEquals(testExpense1.getPaymentDateTime(), e1.getPaymentDateTime());
+    }
+
+    @Test
+    void testFindByShortDescription() {
+        String shortDescription = testExpense1.getDescription().substring(3, 12);
+        List<Expense> elist2 =
+            expenseRepository.findByDescriptionContainingIgnoreCase(shortDescription);
+
+        Expense e2 = elist2.get(0);
+        assertNotNull(e2);
+
+        assertEquals(testExpense1.getDescription(), e2.getDescription());
+        assertEquals(testExpense1.getPaid(), e2.getPaid());
+        assertEquals(testExpense1.getReceiver(), e2.getReceiver());
+        assertEquals(testExpense1.getParticipants(), e2.getParticipants());
+        assertEquals(testExpense1.getDescription(), e2.getDescription());
+        assertEquals(testExpense1.getPaymentDateTime(), e2.getPaymentDateTime());
     }
 }
