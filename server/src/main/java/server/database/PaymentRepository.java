86c022b64f74ad268506065b75778acdeebc1ff8
Eva Miesen <e.a.p.miesen@student.tudelft.nl>
2024-03-18 07:56:33 +0100

    Added queries in PaymentRepository class

--- server/src/main/java/server/database/PaymentRepository.java
+++ server/src/main/java/server/database/PaymentRepository.java
@@ -1,4 +1,17 @@
 package server.database;
 
-public interface PaymentRepository {
+import commons.Payment;
+import commons.Person;
+import java.math.BigDecimal;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The repository interface of the Payment class.
+ */
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByAmount(BigDecimal amount);
+    List<Payment> findByReceiver(Person receiver);
+    List<Payment> findByPayer(Person payer);
 }
