00de52378b8c0bb08d60c2096a3e285faa58c8ec
Eva Miesen <e.a.p.miesen@student.tudelft.nl>
2024-03-19 11:15:43 +0100

    Fixed checkstyle

--- server/src/test/java/server/database/PaymentRepositoryTest.java
+++ server/src/test/java/server/database/PaymentRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import commons.Payment;
 import commons.Person;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
