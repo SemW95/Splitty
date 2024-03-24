978fce4be5715bf2ae4e09d93866c5b4c7d9ed36
Rens Pols <G.J.Pols@student.tudelft.nl>
2024-03-24 20:38:29 +0100

    Removed TODO #65

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -220,7 +220,6 @@ public class Event {
     }
 
     public void updateLastModifiedDateTime() {
-        // TODO: update the lastModifiedDateTime variable
         this.lastModifiedDateTime = Instant.now();
     }
 
