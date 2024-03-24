85710570602e952830b7cc9cd4cbdb35409af434
Xiaoyu Du <X.Du-9@student.tudelft.nl>
2024-03-24 13:53:25 +0100

    Change the emailCheck in Person to a more extensive one.

--- commons/src/main/java/commons/Person.java
+++ commons/src/main/java/commons/Person.java
@@ -116,9 +116,8 @@ public class Person {
         return checkSum % 97 == 1;
     }
 
-
     /**
-     * TODO: Should use BIC api, to verify BIC.
+     * Check if BIC is valid.
      *
      * @param bic takes a bic number
      * @return a boolean if it is a correct/existing bic.
@@ -132,15 +131,18 @@ public class Person {
     }
 
     /**
-     * Checks email validity.
+     * Check if the email address is valid.
      *
-     * @param email an email to test for validity
-     * @return whether the email is valid,
+     * @param email The email address that need to checked.
+     * @return True if it is valid, false otherwise.
      */
     public static boolean emailCheck(String email) {
-        return email.matches("^(.+)@(\\S+)$");
+        String emailRegex = "^[a-zA-Z0-9_+&*-]"
+                + "+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
-
 
     @Override
     public String toString() {
