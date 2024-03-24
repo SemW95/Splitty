d6e1b6a648e1f69013bee8b9f86360e056e59412
Eva Miesen <e.a.p.miesen@student.tudelft.nl>
2024-03-24 16:00:20 +0100

    Added EventOverview.fxml file + changed size in Home.fxml

--- server/src/test/java/server/database/PaymentRepositoryTest.java
+++ server/src/test/java/server/database/PaymentRepositoryTest.java
@@ -13,7 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-class PaymentRepositoryTest {
+class
+
+
+PaymentRepositoryTest {
 
     @Autowired
     private PaymentRepository paymentRepository;
