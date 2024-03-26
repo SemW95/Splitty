45979e52fd1c1b8681e9b8438f6b50bcfc04f1cb
Rens Pols <G.J.Pols@student.tudelft.nl>
2024-03-26 22:15:27 +0100

    Added the PaymentService.java methods #72

---
+++ server/src/main/java/server/service/PaymentService.java
@@ -0,0 +1,134 @@
+package server.service;
+
+import commons.Payment;
+import commons.Person;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import server.database.PaymentRepository;
+
+/**
+ * Service for Tag. [CONT -> SERV -> REPO]
+ */
+@Service
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public void addPayment(Payment payment) {
+        Optional<Payment> optionalPayment = paymentRepository.findById(payment.getId());
+
+        if (optionalPayment.isPresent()) {
+            throw new IllegalStateException("A Payment with this id already exists");
+        }
+
+        paymentRepository.save(payment);
+    }
+
+    /** Returns all payments.
+     *
+     * @return All payments
+     */
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    /** Returns a Payment Object for a given id.
+     *
+     * @param id The id of the Payment that should be received
+     * @return The specified Payment
+     */
+    public Payment getPaymentById(Long id) {
+        Optional<Payment> optionalPayment = paymentRepository.findById(id);
+
+        if (optionalPayment.isEmpty()) {
+            throw new IllegalStateException("There is no Payment with this id");
+        }
+
+        return optionalPayment.get();
+    }
+
+    /** Retrieves the (Person) payer of the specified Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @return The payer Person Object in the Payment
+     */
+    public Person getPayer(Long paymentId) {
+        return getPaymentById(paymentId).getPayer();
+    }
+
+    /** Retrieves the (Person) receiver of the specified Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @return The receiver Person Object in the Payment
+     */
+    public Person getReceiver(Long paymentId) {
+        return getPaymentById(paymentId).getReceiver();
+    }
+
+
+    /** Retrieves the (BigDecimal) amount of the specified Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @return The amount BigDecimal Object in the Payment
+     */
+    public BigDecimal getAmount(Long paymentId) {
+        return getPaymentById(paymentId).getAmount();
+    }
+
+    /** Sets a new payer for a Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @param payer The payer for the Payment
+     */
+    public void setPayer(Long paymentId, Person payer) {
+        Payment payment = getPaymentById(paymentId);
+
+        payment.setPayer(payer);
+        paymentRepository.save(payment);
+    }
+
+    /** Sets a new receiver for a Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @param receiver The receiver for the Payment
+     */
+    public void setReceiver(Long paymentId, Person receiver) {
+        Payment payment = getPaymentById(paymentId);
+
+        payment.setReceiver(receiver);
+        paymentRepository.save(payment);
+    }
+
+    /** Sets a new amount for a Payment.
+     *
+     * @param paymentId The id of the Payment
+     * @param amount The amount for the Payment
+     */
+    public void setAmount(Long paymentId, BigDecimal amount) {
+        Payment payment = getPaymentById(paymentId);
+
+        payment.setAmount(amount);
+        paymentRepository.save(payment);
+    }
+
+    /** Deletes a Payment.
+     *
+     * @param id The id of the Payment that should be deleted
+     */
+    public void deletePayment(Long id) {
+        Optional<Payment> optionalPayment = paymentRepository.findById(id);
+
+        if (optionalPayment.isEmpty()) {
+            throw new IllegalStateException("There is no Payment with this id");
+        }
+
+        paymentRepository.deleteById(id);
+    }
+}
