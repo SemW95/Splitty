2707d8eb1eb378d9c639bd11ff3d9410a30ef961
Rens Pols <G.J.Pols@student.tudelft.nl>
2024-04-02 16:27:27 +0200

    Made the code null-safe

--- server/src/main/java/server/service/EventService.java
+++ server/src/main/java/server/service/EventService.java
@@ -20,6 +20,15 @@ public class EventService {
         this.eventRepository = eventRepository;
     }
 
+    /** Checks if an Event with the id of the given Event is already in the database.
+     *
+     * @param event The Event for which the id that should be checked
+     * @return true if an Event with the same id is present
+     */
+    private boolean eventExists(Event event) {
+        return eventRepository.existsById(event.getId());
+    }
+
     public List<Event> getAllEvent() {
         return eventRepository.findAll();
     }
@@ -30,7 +39,7 @@ public class EventService {
      * @return Event that was searched
      * @throws IllegalStateException When no Event with the given id is present
      */
-    public Event getEventById(String id) {
+    public Event getEventById(String id) throws IllegalStateException {
         Optional<Event> optionalEvent = eventRepository
             .findById(id);
 
@@ -45,14 +54,14 @@ public class EventService {
      *
      * @param code of the event
      * @return Event that was searched
-     * @throws IllegalStateException When no Event with the given id is present
+     * @throws IllegalStateException When no Event with the given code is present
      */
     public Event getEventByCode(String code) throws IllegalStateException {
         Optional<Event> optionalEvent = eventRepository
             .findByCode(code);
 
         if (optionalEvent.isEmpty()) {
-            throw new IllegalStateException("There is no Event with this id");
+            throw new IllegalStateException("There is no Event with this code");
         }
 
         return optionalEvent.get();
@@ -61,10 +70,11 @@ public class EventService {
     /** Deletes an Event based on its id.
      *
      * @param id The id of the Event that should be deleted
+     * @throws IllegalStateException When there isn't an Event with this id
      */
-    public void deleteEvent(String id) {
+    public void deleteEvent(String id) throws IllegalStateException {
         if (eventRepository.existsById(id)) {
-            throw new IllegalStateException("There isn't an Event with this code");
+            throw new IllegalStateException("There isn't an Event with this id");
         }
 
         eventRepository.deleteById(id);
@@ -74,15 +84,25 @@ public class EventService {
      * Creates an event. Fails if an event exists with the same id or invite code.
      *
      * @param event the event to be created.
+     * @throws IllegalStateException When there already is an Event with this id
      */
-    public Event createEvent(Event event) {
+    public Event createEvent(Event event) throws IllegalStateException {
         if (event.getId() == null || !eventRepository.existsById(event.getId())) {
             return eventRepository.save(event);
         }
-        return null;
+        throw new IllegalStateException("There already is an Event with this id");
     }
 
-    public void updateEvent(Event event) {
+    /** Updates an already existing Event.
+     *
+     * @param event The Event that should be updated
+     * @throws IllegalStateException When there isn't an Event with this id
+     */
+    public void updateEvent(Event event) throws IllegalStateException {
+        if (!eventExists(event)) {
+            throw new IllegalStateException("There isn't an Event with this code");
+        }
+
         eventRepository.save(event);
     }
 }
