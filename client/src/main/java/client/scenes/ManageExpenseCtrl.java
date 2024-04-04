28262630c88bb752f55c05112f83d50c75166c51
Wing Wong <w.p.wong@student.tudelft.nl>
2024-04-04 18:54:50 +0200

    Keyboard Navigation Escape 100%, Enter 100%, Shift-Tab 80%

--- client/src/main/java/client/scenes/ManageExpenseCtrl.java
+++ client/src/main/java/client/scenes/ManageExpenseCtrl.java
@@ -19,7 +19,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
