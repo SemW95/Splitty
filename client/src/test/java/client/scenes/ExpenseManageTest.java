cf1314e4d13bf5c3daa2ff754912978d5a35ebe4
Wing Wong <w.p.wong@student.tudelft.nl>
2024-04-14 19:58:04 +0200

    finalize test clientside expense

--- client/src/test/java/client/scenes/ExpenseManageTest.java
+++ client/src/test/java/client/scenes/ExpenseManageTest.java
@@ -1,22 +1,24 @@
 package client.scenes;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import client.Main;
-import java.util.Optional;
+import java.util.ResourceBundle;
 import javafx.application.Application;
-import javafx.scene.Node;
-import javafx.scene.text.Text;
+import javafx.scene.input.KeyCode;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 
 /**
  * Main test.
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExpenseManageTest extends FxRobot {
     private static Application application;
 
@@ -29,17 +31,151 @@ public class ExpenseManageTest extends FxRobot {
     @AfterAll
     public static void cleanup() throws Exception {
         FxToolkit.cleanupApplication(application);
+        FxToolkit.cleanupStages();
     }
 
+    @Order(1)
     @Test
-    public void eventNamesTest() {
-        assertTrue(lookup("Just food with friends").tryQuery().isPresent());
-        assertTrue(lookup("Celebration Dinner").tryQuery().isPresent());
-        assertTrue(lookup("Uber ride").tryQuery().isPresent());
-        assertTrue(lookup("Paintball").tryQuery().isEmpty());
-        String recentOverview = Main.FXML.getBundle().getString("home.recent-overview");
-        Optional<Node> recentEventOverviewNode = lookup("#recentOverview").tryQuery();
-        assertTrue(recentEventOverviewNode.isPresent());
-        assertEquals(((Text) recentEventOverviewNode.get()).getText(), recentOverview);
+    public void eventCreateTest() {
+        ResourceBundle bundle = Main.FXML.getBundle();
+        clickOn(bundle.getString("home.create-event"));
+
+        assertTrue(lookup(bundle.getString("create-event.create-event")).tryQuery().isPresent());
+
+        clickOn("#eventName").type(KeyCode.A, KeyCode.B);
+        clickOn("#eventDescription").type(KeyCode.B, KeyCode.C);
+        clickOn("#create");
+
+        assertTrue(lookup("ab").tryQuery().isPresent());
+
+        sleep(500);
+        type(KeyCode.ESCAPE);
+        sleep(500);
+        type(KeyCode.SPACE);
+        sleep(500);
+        type(KeyCode.ESCAPE);
+
+
     }
+
+    @Order(2)
+    @Test
+    public void renameTest() {
+        sleep(500);
+        ResourceBundle bundle = Main.FXML.getBundle();
+
+
+        clickOn(bundle.getString("home.create-event"));
+
+        assertTrue(lookup(bundle.getString("create-event.create-event")).tryQuery().isPresent());
+
+        doubleClickOn("#eventName").type(KeyCode.A, KeyCode.B);
+        doubleClickOn("#eventDescription").type(KeyCode.B, KeyCode.C);
+        clickOn("#create");
+
+        sleep(500);
+
+        assertTrue(lookup("ab").tryQuery().isPresent());
+
+        clickOn("#addExpenseButton");
+        clickOn("#expenseNameLabel").type(KeyCode.E, KeyCode.X);
+        press(KeyCode.ENTER);
+        release(KeyCode.ENTER);
+        sleep(500);
+        String goal = bundle.getString("manage-expense.changed");
+        assertTrue(lookup(goal).tryQuery().isPresent());
+
+        type(KeyCode.ESCAPE);
+        sleep(500);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        sleep(500);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+    }
+
+    @Order(3)
+    @Test
+    public void amountChangeTest() {
+        sleep(500);
+        ResourceBundle bundle = Main.FXML.getBundle();
+        clickOn(bundle.getString("home.create-event"));
+
+        assertTrue(lookup(bundle.getString("create-event.create-event")).tryQuery().isPresent());
+
+        doubleClickOn("#eventName").type(KeyCode.A, KeyCode.B);
+        doubleClickOn("#eventDescription").type(KeyCode.B, KeyCode.C);
+        clickOn("#create");
+
+        assertTrue(lookup("ab").tryQuery().isPresent());
+
+        clickOn("#addExpenseButton");
+        clickOn("#expenseAmountLabel");
+        write("7321");
+        press(KeyCode.ENTER);
+
+        String goal = bundle.getString("manage-expense.changed");
+        assertTrue(lookup(goal).tryQuery().isPresent());
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+    }
+
+    @Order(4)
+    @Test
+    public void invalidSyntaxAmountTest() {
+        ResourceBundle bundle = Main.FXML.getBundle();
+        clickOn(bundle.getString("home.create-event"));
+
+        assertTrue(lookup(bundle.getString("create-event.create-event")).tryQuery().isPresent());
+
+        doubleClickOn("#eventName").type(KeyCode.A, KeyCode.B);
+        doubleClickOn("#eventDescription").type(KeyCode.B, KeyCode.C);
+        clickOn("#create");
+
+        assertTrue(lookup("ab").tryQuery().isPresent());
+
+        clickOn("#addExpenseButton");
+        clickOn("#expenseAmountLabel");
+        write("712.five");
+        press(KeyCode.ENTER);
+
+        String goal = bundle.getString("manage-expense.invalid");
+        assertTrue(lookup(goal).tryQuery().isPresent());
+
+        press(KeyCode.ENTER);
+        doubleClickOn("#expenseAmountLabel");
+
+        write("712");
+        press(KeyCode.ENTER);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+
+        type(KeyCode.ESCAPE);
+        type(KeyCode.SPACE);
+        sleep(500);
+    }
+
 }
